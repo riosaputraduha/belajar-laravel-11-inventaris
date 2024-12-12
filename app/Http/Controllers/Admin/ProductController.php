c43d967f84774c0bd3643d8a3841772a7dc5ad6f
riosaputraduha <doehario@gmail.com>
2024-12-12 13:26:47 +0700

    View Data Produk

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        /**
+         * cara 1
+         * $products = DB::table('products')->get();
+         * cara 2
+         */
+        $products = Product::with('category')->get();
+
+        return view('pages.products.index', [
+            "products" => $products,
+        ]);
+    }
+}
---
+++ resources/views/pages/products/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.main')
+
+@section('header')
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1>Produk</h1>
+        </div>
+        <div class="col-sm-6">
+            <ul class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item active"><a href="#">Beranda</a></li>
+                <li class="breadcrumb-item active">Produk</li>
+            </ul>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Produk</th>
+                                <th>Deskripsi</th>
+                                <th>Kode</th>
+                                <th>Harga</th>
+                                <th>Stok</th>
+                                <th>Kategori</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($products as $product)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $product->name }}</td>
+                                    <td>{{ $product->description }}</td>
+                                    <td>{{ $product->sku }}</td>
+                                    <td>{{ $product->price }}</td>
+                                    <td>{{ $product->stock }}</td>
+                                    <td>{{ $product->category->name }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
