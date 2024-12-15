5f18ca90735f956ef57d807f32d86aca1bc52db6
riosaputraduha <doehario@gmail.com>
2024-12-15 12:07:47 +0700

    Tambahkan ketika form kosong disimpan, akan ada pesan error required

--- resources/views/pages/products/create.blade.php
+++ resources/views/pages/products/create.blade.php
@@ -24,31 +24,49 @@
                     <div class="card-body">
                         <div class="form-group">
                             <label for="name" class="form-label">Nama Produk</label>
-                            <input type="text" name="name" id="name" class="form-control">
+                            <input type="text" name="name" id="name" class="form-control @error('name') is invalid @enderror">
+                            @error('name')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="description" class="form-label">Deskripsi</label>
-                            <textarea name="description" id="description" cols="30" rows="10" class="form-control"></textarea>
+                            <textarea name="description" id="description" cols="30" rows="10" class="form-control @error('description') is invalid @enderror"></textarea>
+                            @error('description')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="sku" class="form-label">Kode Produk</label>
-                            <input type="text" name="sku" id="sku" class="form-control">
+                            <input type="text" name="sku" id="sku" class="form-control @error('sku') is invalid @enderror">
+                            @error('sku')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="price" class="form-label">Harga</label>
-                            <input type="number" inputmode="numeric" name="price" id="price" class="form-control">
+                            <input type="number" inputmode="numeric" name="price" id="price" class="form-control @error('price') is invalid @enderror">
+                            @error('price')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="stock" class="form-label">Stok</label>
-                            <input type="number" inputmode="numeric" name="stock" id="stock" class="form-control">
+                            <input type="number" inputmode="numeric" name="stock" id="stock" class="form-control @error('stock') is invalid @enderror">
+                            @error('stock')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="category_id" class="form-label">Kategori</label>
-                            <select name="category_id" id="category_id" class="form-control">
+                            <select name="category_id" id="category_id" class="form-control  @error('category_id') is invalid @enderror">
                                 @foreach ($categories as $category)
                                     <option value="{{ $category->id }}">{{ $category->name }}</option>
                                 @endforeach
                             </select>
+                            @error('category_id')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
                         </div>
                     </div>
                     <div class="card-footer">
