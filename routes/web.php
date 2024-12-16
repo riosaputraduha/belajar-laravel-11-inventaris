15b87d5699d9bd17b19d5abea222c9fe514f40b3
riosaputraduha <doehario@gmail.com>
2024-12-17 01:23:00 +0700

    Menambahkan middleware untuk memastikan pengguna sudah login sebelum mengakses halaman tertentu

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,27 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Middleware\IsLogin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'loginView']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::get('/', [DashboardController::class, 'index']);
-
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/create', [ProductController::class, 'create']);
-Route::get('/products/edit/{id}', [ProductController::class, 'edit']);
-Route::post('/products/store', [ProductController::class, 'store']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'delete']);
-
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/create', [CategoryController::class, 'create']);
-Route::get('/categories/edit/{id}', [CategoryController::class, 'edit']);
-Route::post('/categories/store', [CategoryController::class, 'store']);
-Route::put('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
+Route::middleware(IsLogin::class)->group(function() {
+    Route::get('/', [DashboardController::class, 'index']);
+    
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/create', [ProductController::class, 'create']);
+    Route::get('/products/edit/{id}', [ProductController::class, 'edit']);
+    Route::post('/products/store', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'delete']);
+    
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories/create', [CategoryController::class, 'create']);
+    Route::get('/categories/edit/{id}', [CategoryController::class, 'edit']);
+    Route::post('/categories/store', [CategoryController::class, 'store']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
+});
