cd4ab12747566677a688bbdbba9f65f11ecbd927
riosaputraduha <doehario@gmail.com>
2025-01-06 13:52:44 +0700

    membuat fitur total produk dan kategori pada halaman dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard.admin');
+        $productCount = Product::count();
+        $categoryCount = Category::count();
+
+        return view('pages.dashboard.admin', compact('productCount', 'categoryCount'));
     }
 }
