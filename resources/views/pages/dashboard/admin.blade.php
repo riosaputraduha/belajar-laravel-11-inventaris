d5f08ff9d93bf5fa9412d4fc46f8b2cee230a7a6
riosaputraduha <doehario@gmail.com>
2025-01-16 00:50:27 +0700

    update link

--- resources/views/pages/dashboard/admin.blade.php
+++ resources/views/pages/dashboard/admin.blade.php
@@ -16,35 +16,35 @@
 
 @section('content')
 <div class="row">
-    <div class="col-lg-3 col-6">
-      <!-- small box -->
-      <div class="small-box bg-info">
-        <div class="inner">
-          <h3>{{ $productCount }}</h3>
-
-          <p>Produk</p>
-        </div>
-        <div class="icon">
-          <i class="ion ion-bag"></i>
-        </div>
-        <a href="#" class="small-box-footer">Info lebih lanjut <i class="fas fa-arrow-circle-right"></i></a>
+  <div class="col-lg-3 col-6">
+    <!-- small box -->
+    <div class="small-box bg-success">
+      <div class="inner">
+        <h3>{{ $categoryCount }}</h3>
+        
+        <p>Total Kategori</p>
       </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-      <!-- small box -->
-      <div class="small-box bg-success">
-        <div class="inner">
-          <h3>{{ $categoryCount }}</h3>
-
-          <p>Kategori</p>
-        </div>
-        <div class="icon">
-          <i class="ion ion-stats-bars"></i>
-        </div>
-        <a href="#" class="small-box-footer">Info lebih lanjut <i class="fas fa-arrow-circle-right"></i></a>
+      <div class="icon">
+        <i class="ion ion-stats-bars"></i>
       </div>
+      <a href="{{ route('categories.index') }}" class="small-box-footer">Info lebih lanjut <i class="fas fa-arrow-circle-right"></i></a>
     </div>
-    <!-- ./col -->
+  </div>
+  <!-- ./col -->
+  <div class="col-lg-3 col-6">
+    <!-- small box -->
+    <div class="small-box bg-info">
+      <div class="inner">
+        <h3>{{ $productCount }}</h3>
+
+        <p>Total Produk</p>
+      </div>
+      <div class="icon">
+        <i class="ion ion-bag"></i>
+      </div>
+      <a href="{{ route('products.index') }}" class="small-box-footer">Info lebih lanjut <i class="fas fa-arrow-circle-right"></i></a>
+    </div>
+  </div>
+  <!-- ./col -->
   </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,14 @@ Route::post('/logout', [AuthController::class, 'logout']);
 Route::get('/', [DashboardController::class, 'index']);
 
 Route::middleware(IsLogin::class)->group(function() {
-    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
     Route::get('/products/create', [ProductController::class, 'create']);
     Route::get('/products/edit/{id}', [ProductController::class, 'edit']);
     Route::post('/products/store', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'delete']);
     
-    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
     Route::get('/categories/create', [CategoryController::class, 'create']);
     Route::get('/categories/edit/{id}', [CategoryController::class, 'edit']);
     Route::post('/categories/store', [CategoryController::class, 'store']);
